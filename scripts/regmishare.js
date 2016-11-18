6c17b0373c3aa02453d8742ab1c9d0eb4785308c
Justin <justin-sethi@hotmail.com>
2016-11-18 18:27:25 -0500

    Initial commit

---
+++ scripts/regmishare.js
@@ -0,0 +1,38 @@
+function attemptLogin(){
+
+	var	username = document.login.username.value;
+	var	password = document.login.password.value;
+	
+	var uExp = /^[\w]+$/;
+	var pExp = /^[\x00-\x7F]+$/;
+	
+	if(username.length < 6 || username.length > 16 || !username.match(uExp)){
+		confirm("Username must be alphanumeric (underscores allowed) and 6 to 16 characters in length");
+		return false;
+	}
+	
+	if(password.length < 6 || password.length > 16 || !password.match(pExp)){
+		confirm("Password must be 6 to 16 characters in length");
+		return false;
+	}
+	password = encodeURIComponent(password);
+
+	$.ajax({
+	  type: "POST",
+	  url: "scripts/verifyLogin.php",
+	  data: 'postusername='+username+'&postpassword='+password,
+	  datatype: "html",
+	  async: false,
+	  success: function(result){
+
+	      if(result == 1){
+				confirm("Logged In");
+			} else if(result == 0){
+				confirm("New Account Created With Entered Credentials");
+			} else{
+				confirm(result);
+			}  
+	      }
+  	})
+
+}
---
+++ scripts/verifyLogin.php
@@ -0,0 +1,101 @@
+<?php
+
+$ini_array = parse_ini_file("databaseInfo.ini");
+
+$sqlservername = $ini_array["sqlservername"];
+$sqlusername = $ini_array["sqlusername"];
+$sqlpassword = $ini_array["sqlpassword"];
+$sqldatabase = $ini_array["sqldatabase"];
+
+function pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output = false)
+{
+    $algorithm = strtolower($algorithm);
+    if(!in_array($algorithm, hash_algos(), true))
+        trigger_error('PBKDF2 ERROR: Invalid hash algorithm.', E_USER_ERROR);
+    if($count <= 0 || $key_length <= 0)
+        trigger_error('PBKDF2 ERROR: Invalid parameters.', E_USER_ERROR);
+
+    if (function_exists("hash_pbkdf2")) {
+        // The output length is in NIBBLES (4-bits) if $raw_output is false!
+        if (!$raw_output) {
+            $key_length = $key_length * 2;
+        }
+        return hash_pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output);
+    }
+
+    $hash_length = strlen(hash($algorithm, "", true));
+    $block_count = ceil($key_length / $hash_length);
+
+    $output = "";
+    for($i = 1; $i <= $block_count; $i++) {
+        // $i encoded as 4 bytes, big endian.
+        $last = $salt . pack("N", $i);
+        // first iteration
+        $last = $xorsum = hash_hmac($algorithm, $last, $password, true);
+        // perform the other $count - 1 iterations
+        for ($j = 1; $j < $count; $j++) {
+            $xorsum ^= ($last = hash_hmac($algorithm, $last, $password, true));
+        }
+        $output .= $xorsum;
+    }
+
+    if($raw_output)
+        return substr($output, 0, $key_length);
+    else
+        return bin2hex(substr($output, 0, $key_length));
+}
+
+// If bad information is somehow entered, handle it
+$username = $_POST['postusername'];
+$password = filter_var(mysql_escape_string($_POST['postpassword']));
+
+//debug
+//echo json_encode($password);
+//mysql_close($con);
+
+$regex = "/^[\w]{6,16}$/";
+$regexp = "/^[\\x00-\\x7F]{6,16}$/";
+
+if(!preg_match($regex, $username)){
+	$loginStatus = "Username must be alphanumeric (underscores allowed) and 6 to 16 characters in length";
+}
+else if(!preg_match($regexp, $password)){
+	$loginStatus = "Password must be 6 to 16 characters in length";
+}
+else{ 
+	$con = new mysqli($sqlservername,$sqlusername,$sqlpassword,$sqldatabase); 
+	
+    //RETRIEVE USER INFO FROM DATABASE
+	$query = sprintf("SELECT * FROM userInfo WHERE username = '%s';", $username);
+	$result = $con->query($query);
+	$row = $result->fetch_assoc();
+	$usr = $row["username"];
+	$db_pw = $row["pass"];
+    $dbsalt = $row["salt"];
+
+    $hashed_password = pbkdf2("sha256", $password, $dbsalt, 1000, 32); 
+
+	if(strcasecmp($username, $usr)==0 && $hashed_password == $db_pw){
+		$loginStatus = 1;
+	} else if($hashed_password != $db_pw){
+		if($username != $usr){
+			//SALT GENERATION
+			$salt = openssl_random_pseudo_bytes (32); 
+            $hexsalt = bin2hex($salt); 
+
+            //HASH PASSWORD
+            $hash = pbkdf2("sha256", $password, $hexsalt, 1000, 32);
+
+            //INSERT PASSWORD HASH AND SALT INTO DATABASE
+            $query = "INSERT INTO userInfo(username,pass,salt) VALUES('".$username."','".$hash."','".$hexsalt."');";
+            $con->query($query);
+
+            $loginStatus = 0;
+		} else{
+			$loginStatus = "Incorrect password!";
+		}
+	}
+	$con->close();
+}
+echo json_encode($loginStatus);
+?>
